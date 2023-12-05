6793848846c34f0dd784033d8a9140d8d32c25c3
burillu <burillu90@hotmail.it>
2023-12-05 15:14:59 +0100

    scaffolding by oop-php-1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include __DIR__ . "/partials/header.php";
+include __DIR__ . "/Model/Movie.php";
+
+?>
+
+<div class="container">
+  <div class="row">
+    <?php foreach ($movies as $movie) {
+      echo $movie->printCard();
+      //var_dump($movie);
+    } ?>
+  </div>
+
+</div>
+
+<?php
+include __DIR__ . "/partials/footer.php";
+?>
